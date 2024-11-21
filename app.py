bb3ae93389cb4b939ef514fa6987fab3c6f7c8b1
Step <stphnlines@gmail.com>
2024-11-21 10:17:20 -0800

    added functionality

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
-
+import keys
 app = Flask(__name__)
 @app.route("/")
 def index():
+    do_cool_things()
     fruits = [
         {"name": "apples", "quantity": 3},
-            {"name": "oranges", "quantity": 2},
-            {"name": "strawberries", "quantity": 6}
+        {"name": "oranges", "quantity": 2},
+        {"name": "strawberries", "quantity": 6}
     ]
-    return render_template("index.html", fruits=fruits)
+
+    newfruits = []
+    for fruit in fruits:
+        if fruit["quantity"] <= 3 and fruit["name"].startswith('o'):
+            print(fruit["name"])
+            newfruits.append(fruit)
+            
+    return render_template("index.html", fruits=newfruits)
+
+def do_cool_things():
+    print("I print the API key here, without having it in this file: ", keys.MY_SECRET_API_KEY_1)
